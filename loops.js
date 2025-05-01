758dc4a47202b6404ffa1f9403a5012c0eb97b2c
error7buddy <fxosman7@gmail.com>
2025-05-01 19:08:16 +0600

    2nd Push

---
+++ loops.js
@@ -0,0 +1,38 @@
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+let j = 0;
+while (j < 5) {
+    console.log(j);
+    j++;
+}
+/*
+let k = 0;
+do {
+    console.log(k);
+    k++;
+} while (k < 5);
+for (let i = 0; i < 5; i++) {
+    if (i === 3) {
+        break;
+    }
+    console.log(i);
+}
+
+let j = 0;
+while (j < 5) {
+    if (j === 3) {
+        break;
+    }
+    console.log(j);
+    j++;
+}
+
+let k = 0;
+do {
+    if (k === 3) {
+        break;
+    }
+    console.log(k);
+    k++;
+} while (k < 5);*/
---
+++ string_objects.js
@@ -0,0 +1,20 @@
+const person = {
+    name: "John Doe",
+    age: 30,
+    isStudent: false,
+    hobbies: ["reading", "gaming", "hiking"],
+  };
+  
+  const person2 = {
+    name: "Jane Smith",
+    age: 25,
+    isStudent: true,
+    hobbies: ["painting", "cooking", "traveling"],
+  };
+  
+  console.log(person);   // Output: John Doe
+  console.log(person2);  // Output: Jane Smith
+  
+
+
+  
